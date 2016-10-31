530756c31c34ae17bdd6ad5f423e2234522860c8
briandridge <briandridge@gmail.com>
2016-10-30 21:31:16 -0600

    final after unit 11

--- starter-code/main.js
+++ starter-code/main.js
@@ -18,7 +18,33 @@ function createBoard() {
 	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement('div');
 		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
 		board.appendChild(cardElement);
 	}
 }
 
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var isMatch = function(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+
+}
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+		console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src="king.png" alt="King" />";
+	} else {
+		this.innerHTML = "<img src="queen.png" alt="queen" />";
+	}
+}
+
+createBoard();
